0b2545813badba4ba1640639869f58a7e4379d6d
Matan Shiloni <mshiloni22@gmail.com>
2021-05-25 21:42:42 +0300

    nothing

--- cluster.py
+++ cluster.py
@@ -17,7 +17,7 @@ class Cluster:
         id_list = []
         labels = {'B - CELL_ALL': 0, 'B - CELL_ALL_TCF3 - PBX1': 0, 'B - CELL_ALL_HYPERDIP': 0, 'B - CELL_ALL_HYPO': 0,
                   'B - CELL_ALL_MLL': 0, 'B - CELL_ALL_T - ALL': 0, 'B - CELL_ALL_ETV6 - RUNX1': 0}
-        ## אולי צריך "" במקום ''
+        # אולי צריך "" במקום ''
         for s in self.samples:
             labels[s.label] += 1
             id_list.append(s.s_id)
@@ -31,11 +31,3 @@ class Cluster:
                     dominant_label = i
 
         print("Cluster", self.c_id, ":[", id_list, "], dominant label =", dominant_label, ", silhouette =", silhouette)
-
-
-
-
-
-
-
-
