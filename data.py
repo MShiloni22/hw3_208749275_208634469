572b8f362c8de0ea3773e983b1d75ecd540bccbd
Matan Shiloni <mshiloni22@gmail.com>
2021-05-25 21:29:11 +0300

    finished module data

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pandas
+import sample
 
 
 # define Data class
@@ -11,3 +12,18 @@ class Data:
         self.path = path
         df = pandas.read_csv(path)
         self.data = df.to_dict(orient="list")
+
+    def create_samples(self):
+        """
+        creates a list of all the samples, based on the dataset
+        :return: a list of all the samples
+        """
+        samples = []
+        genes_list = []
+        keys_list = self.data.keys()
+        for i in self.data["samples"]:
+            for j in keys_list:
+                if j != "samples" and j != "type":
+                    genes_list.append(self.data[j][i])
+            new_sample = sample.Sample(self.data["samples"][i], genes_list, self.data["type"][i])
+            samples.append(new_sample)
