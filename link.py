2b2fb3d8096e57d24ca619993a2b15b2637345d4
Matan Shiloni <mshiloni22@gmail.com>
2021-06-13 20:12:52 +0300

    implemented compute method, in both single and complete link

--- link.py
+++ link.py
@@ -1,3 +1,6 @@
+from sample import Sample
+
+
 class Link:
     def compute(self, cluster, other):
         # Abstract method, defined by convention only
@@ -6,5 +9,33 @@ class Link:
 
 class SingleLink(Link):
     def compute(self, cluster, other):
-        # need to implement!
-        print("hello")
+        """
+        computes the distance between two clusters, according to single link method
+        :param cluster: first cluster
+        :param other: second cluster
+        :return: the distance between the clusters, according to single link method
+        """
+        minimum_distance = Sample.compute_euclidean_distance(cluster.samples[0], other.samples[0])
+        for point_from_cluster in cluster.samples:
+            for point_from_other in other.samples:
+                distance = Sample.compute_euclidean_distance(point_from_cluster, point_from_other)
+                if distance < minimum_distance:
+                    minimum_distance = distance
+        return minimum_distance
+
+
+class CompleteLink(Link):
+    def compute(self, cluster, other):
+        """
+                computes the distance between two clusters, according to single link method
+                :param cluster: first cluster
+                :param other: second cluster
+                :return: the distance between the clusters, according to single link method
+                """
+        maximum_distance = Sample.compute_euclidean_distance(cluster.samples[0], other.samples[0])
+        for point_from_cluster in cluster.samples:
+            for point_from_other in other.samples:
+                distance = Sample.compute_euclidean_distance(point_from_cluster, point_from_other)
+                if distance > maximum_distance:
+                    maximum_distance = distance
+        return maximum_distance
