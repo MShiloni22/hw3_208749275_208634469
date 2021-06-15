bf72f6616bb45599becc913db1f8cd9d6ca5998b
Matan Shiloni <mshiloni22@gmail.com>
2021-06-15 12:36:36 +0300

    build main for the first time - before testing

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import sys
+import data
+import link
+import clustering_agglomerative
 
 
 def main(argv):
+    data_dict = data.Data(argv[1])
+    samples_list = data_dict.create_samples()
+    distance_list = data_dict.create_distance_matrix()
+    max_clusters = 7
+
+    print("single link:")
+    single_link = link.SingleLink
+    runner_single = clustering_agglomerative.AgglomerativeClustering(single_link, samples_list)
+    runner_single.run(max_clusters, distance_list)
+    print()
+
+    print("complete link:")
+    complete_link = link.CompleteLink
+    runner_complete = clustering_agglomerative.AgglomerativeClustering(complete_link, samples_list)
+    runner_complete.run(max_clusters, distance_list)
 
 
 
