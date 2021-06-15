2ee233003a0f6ba5d50e2a536f537553f3175d1b
Matan Shiloni <mshiloni22@gmail.com>
2021-06-15 15:31:11 +0300

    changes after test 16 ready for test 17

--- cluster.py
+++ cluster.py
@@ -41,8 +41,8 @@ class Cluster:
         count = 0
         dominant_label = ""
         id_list = []
-        labels = {"B - CELL_ALL": 0, "B - CELL_ALL_TCF3 - PBX1": 0, "B - CELL_ALL_HYPERDIP": 0, "B - CELL_ALL_HYPO": 0,
-                  "B - CELL_ALL_MLL": 0, "B - CELL_ALL_T - ALL": 0, "B - CELL_ALL_ETV6 - RUNX1": 0}
+        labels = {"B-CELL_ALL": 0, "B-CELL_ALL_TCF3-PBX1": 0, "B-CELL_ALL_HYPERDIP": 0, "B-CELL_ALL_HYPO": 0,
+                  "B-CELL_ALL_MLL": 0, "B-CELL_ALL_T-ALL": 0, "B-CELL_ALL_ETV6-RUNX1": 0}
         # אולי צריך "" במקום ''
         for s in self.samples:
             labels[s.label] += 1
