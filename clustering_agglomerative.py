1e1414e4c074ce0f536d4a5e18549399e48db9f3
Matan Shiloni <mshiloni22@gmail.com>
2021-06-15 16:12:09 +0300

    changes after test 23 ready for test 24

--- clustering_agglomerative.py
+++ clustering_agglomerative.py
@@ -34,7 +34,7 @@ class AgglomerativeClustering:
                 out = min(out_val)
                 out_val.clear()
                 if out == 0 and in_val == 0:
-                    silhoeutte_samples[s.s_id] = 0
+                    silhoeutte_samples[s.s_id] = round(0, 1)
                     continue
                 silhoeutte_samples[s.s_id] = round((out-in_val)/max(out, in_val), 3)
         return silhoeutte_samples
@@ -52,7 +52,7 @@ class AgglomerativeClustering:
             for sample in cluster.samples:
                 sum_sil = sum_sil + silhoeutte_dict[sample.s_id]
                 if len(cluster.samples) <= 1:
-                    summery_silhoeutte[cluster.c_id] = 0
+                    summery_silhoeutte[cluster.c_id] = round(0, 1)
                 else:
                     summery_silhoeutte[cluster.c_id] = round(sum_sil/len(cluster.samples), 3)
         summery_silhoeutte[0] = silhoeutte_dict
